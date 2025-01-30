a509449765885c6a3fc65ec45dbe84f9d575fa26
Tomek <tomeksamule@gmail.com>
2025-01-30 03:22:00 +0100

    Decoding an encoded message, in class challenge to apply what we just learned

--- decode_start.py
+++ decode_start.py
@@ -1,6 +1,13 @@
+
 alphabet = " abcdefghijklmnopqrstuvwxyz"
 vowel = "aeiou"
 message = ""
 with open("secret.txt") as f:
-    for line in f:
-        print(line) # instead of print, you should start decoding
+    for line in f: # read it line by line
+        n_vowels = 0
+        for c in line:
+            if c in vowel:
+                n_vowels += 1
+        message += alphabet[n_vowels]
+
+print(message)
